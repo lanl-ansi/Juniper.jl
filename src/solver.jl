2caa392176244cdb289f7376923b03c12e41c62b
Ole Kroeger <o.kroeger@wikunia.de>
2017-11-29 00:49:02 +0100

    merged master

--- src/solver.jl
+++ src/solver.jl
@@ -35,7 +35,7 @@ function get_default_options()
     # Traversing    
     traverse_strategy               = :BFS
     return SolverOptions(log_levels,branch_strategy,gain_mu,strong_branching_perc,strong_branching_nsteps,strong_restart,
-        incumbent_constr,obj_epsilon,time_limit,mip_gap,best_obj_stop,solution_limit,all_solutions,
+        reliability_branching_threshold, reliability_branching_perc, incumbent_constr,obj_epsilon,time_limit,mip_gap,best_obj_stop,solution_limit,all_solutions,
         list_of_solutions,processors,traverse_strategy)
 end
 
