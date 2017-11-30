adac3f749969161a31c4f6808a0344d97a958b8c
Ole Kroeger <o.kroeger@wikunia.de>
2017-12-01 00:36:36 +0100

    generate linear expressions once and correct way of avoiding a cycle using a tabu list

--- src/fpump.jl
+++ src/fpump.jl
@@ -15,8 +15,62 @@ type TabuList
     TabuList() = new()
 end
 
+"""
+    construct_affine_vector(m)
 
-function generate_mip(m, nlp_sol, tabu_list)
+Construct a vector of affine expressions for all linear functions using the derivative
+"""
+function construct_affine_vector(m)
+    js = MathProgBase.jac_structure(m.d)
+
+    jg = zeros(length(js[1]))
+    MathProgBase.eval_jac_g(m.d, jg, ones(m.num_var))
+
+    # Construct the data structure for our affine constraints
+    aff = Vector{Aff}(m.num_l_constr)
+    for i=1:m.num_l_constr
+        aff[i] = Aff()
+        aff[i].var_idx = []
+        aff[i].coeff = []
+        constr_expr = MathProgBase.constr_expr(m.d,i)
+        aff[i].rhs = constr_expr.args[3]
+        aff[i].sense = constr_expr.args[1]
+    end
+
+    # if linear constraint the derivative are the coeffs
+    idx = 1
+    lconstr2constr = Vector{Int64}()
+    constr2lconstr = Vector{Int64}()
+    c = 1
+    for i=1:m.num_constr
+        if m.isconstrlinear[i]
+            push!(lconstr2constr,i)
+            push!(constr2lconstr,c)
+            c+=1
+        else
+            push!(constr2lconstr,0)
+        end
+    end
+
+    for row in js[1]
+        if m.isconstrlinear[row]
+            col = js[2][idx]
+            aidx = constr2lconstr[row]
+            push!(aff[row].var_idx, col)
+            push!(aff[row].coeff, jg[idx])
+        end
+        idx += 1
+    end
+    return aff
+end
+
+"""
+    generate_mip(m, nlp_sol, aff, tabu_list)
+
+Generate a mip using the linear constraints (aff) of the original model
+Minimize the distance to nlp_sol and avoid using solutions inside the tabu list
+"""
+function generate_mip(m, nlp_sol, aff, tabu_list)
     mip_model = Model(solver=m.mip_solver)
     lb = [m.l_var; -1e6]
     ub = [m.u_var; 1e6]
@@ -28,73 +82,25 @@ function generate_mip(m, nlp_sol, tabu_list)
             setcategory(mx[i], :Bin)
         end
     end
-
-    g = zeros(m.num_constr)
-    MathProgBase.eval_g(m.d,g,nlp_sol)
-
-    js = MathProgBase.jac_structure(m.d)
-
-    jg = zeros(length(js[1]))
-    MathProgBase.eval_jac_g(m.d, jg, nlp_sol)
-
-    # Construct the data structure for our affine constraints
-    aff = Vector{Aff}(m.num_constr)
-    for i=1:m.num_constr
-        aff[i] = Aff()
-        aff[i].var_idx = []
-        aff[i].coeff = []
-        constr_expr = MathProgBase.constr_expr(m.d,i)
-        aff[i].rhs = constr_expr.args[3]
-        aff[i].sense = constr_expr.args[1]
-    end
-
-    # if linear constraint the derivative are the coeffs
-    idx = 1
-    for row in js[1]
-        if m.isconstrlinear[row]
-            col = js[2][idx]
-            push!(aff[row].var_idx, col)
-            push!(aff[row].coeff, jg[idx])
-        end
-        idx += 1
-    end
     
-    counter = 1
-    linear = false
-    constr_removed = 0
     for c in aff
-        if m.isconstrlinear[counter]
-            if c.sense == :(>=)
-                @constraint(mip_model, sum(mx[c.var_idx[i]]*c.coeff[i] for i=1:length(c.var_idx)) >= c.rhs)
-            elseif c.sense == :(<=)
-                @constraint(mip_model, sum(mx[c.var_idx[i]]*c.coeff[i] for i=1:length(c.var_idx)) <= c.rhs)
-            elseif c.sense == :(==) && linear
-                @constraint(mip_model, sum(mx[c.var_idx[i]]*c.coeff[i] for i=1:length(c.var_idx)) == c.rhs)
-            end
-            counter += 1
+        if c.sense == :(>=)
+            @constraint(mip_model, sum(mx[c.var_idx[i]]*c.coeff[i] for i=1:length(c.var_idx)) >= c.rhs)
+        elseif c.sense == :(<=)
+            @constraint(mip_model, sum(mx[c.var_idx[i]]*c.coeff[i] for i=1:length(c.var_idx)) <= c.rhs)
+        elseif c.sense == :(==)
+            @constraint(mip_model, sum(mx[c.var_idx[i]]*c.coeff[i] for i=1:length(c.var_idx)) == c.rhs)
         end
     end
 
-    @variable(mip_model, mx_p[i=1:m.num_int_bin_var] >= 0)
-    @variable(mip_model, mx_m[i=1:m.num_int_bin_var] >= 0)
+    @variable(mip_model, mabsx[i=1:m.num_int_bin_var] >= 0)
     for i=1:m.num_int_bin_var
         vi = m.int2var_idx[i]
-        @constraint(mip_model, mx[vi] == nlp_sol[vi]+mx_p[i]-mx_m[i])
+        @constraint(mip_model, mabsx[i] >= mx[vi]-nlp_sol[vi])
+        @constraint(mip_model, mabsx[i] >= -mx[vi]+nlp_sol[vi])
     end
-    
-    #=
-        For all discrete i
-        x_{i} => mx[i]
-        |x|   => m.num_int_bin_var
-        x'_{ik} => solution for x[i] in tabu list (pointer -> k)
-        j => the discrete index of i
-        M(1-B_{jk}) ≧ x_{i}-x'_{ik}
-        -M(1-B_{jk}) ≦ x_{i}-x'_{ik} 
-        ∑ B ≦ |x|-1 ∀ k
-        j
-    =#
-    
-    
+      
+    # How long is the tabu list 
     num_sols = 0
     for i=1:tabu_list.length
         if !isnan(tabu_list.sols[i][1]) 
@@ -104,41 +110,35 @@ function generate_mip(m, nlp_sol, tabu_list)
         end
     end
 
+    # If there solutions in the tabu list => avoid them
     if num_sols > 0
         M = 100
-        @variable(mip_model, B[j=1:m.num_int_bin_var,k=1:num_sols], Bin)
+        @variable(mip_model, z1[j=1:m.num_int_bin_var,k=1:num_sols], Bin)
+        @variable(mip_model, z2[j=1:m.num_int_bin_var,k=1:num_sols], Bin)
+        v = tabu_list.sols
         for k=1:num_sols, j=1:m.num_int_bin_var
             i = m.int2var_idx[j] 
-            @constraint(mip_model, M*(1-B[j,k])  >= mx[i]-tabu_list.sols[k][i])
-            @constraint(mip_model, -M*(1-B[j,k]) <= mx[i]-tabu_list.sols[k][i])
+            @constraint(mip_model, z1[j,k]+z2[j,k] <= 1)
+            @constraint(mip_model, (m.l_var[i] - v[k][j])*z1[j,k]+z2[j,k]+v[k][j] <= mx[i])
+            @constraint(mip_model, mx[i] <= v[k][j] - z1[j,k] + (m.u_var[i]-v[k][j])*z2[j,k])
         end
         for k=1:num_sols
-            @constraint(mip_model, sum(B[j,k] for j=1:m.num_int_bin_var) <= m.num_int_bin_var-1)
+            @constraint(mip_model, sum(z1[j,k]+z2[j,k] for j=1:m.num_int_bin_var) >= 1)
         end
-        print(mip_model)
     end
 
-    @objective(mip_model, Min, sum(mx_p[i]+mx_m[i] for i=1:m.num_int_bin_var))
+    @objective(mip_model, Min, sum(mabsx[i] for i=1:m.num_int_bin_var))
     
+    # Break the mip solver if it takes too long or throw a warning when this option isn't available
     try 
         MathProgBase.setparameters!(m.mip_solver, TimeLimit=m.options.feasibility_pump_time_limit)
     catch
-        println("Set parameters is not supported")
+        warn("Set parameters is not supported")
     end
     
     status = solve(mip_model)
-    println("status: ", status)
-    println("Obj: ", getobjectivevalue(mip_model))
-
-    if num_sols > 0
-        for k=1:num_sols, j=1:m.num_int_bin_var 
-            println("B[",j,",",k,"]: ", getvalue(B[j,k]))
-        end
-        for j=1:m.num_int_bin_var
-            i = m.int2var_idx[j]  
-            println("mx[",i,"]: ", getvalue(mx[i]))
-        end
-    end
+    println("MIP Status: ", status)
+    println("MIP Obj: ", getobjectivevalue(mip_model))
 
     # round mip values
     values = getvalue(mx)
@@ -149,6 +149,11 @@ function generate_mip(m, nlp_sol, tabu_list)
     return status, values
 end
 
+"""
+    generate_nlp(m, mip_sol)
+
+Generates the original nlp but changes the objective to minimize the distance to the mip solution
+"""
 function generate_nlp(m, mip_sol)
     nlp_model = Model(solver=m.nl_solver)
     lb = [m.l_var; -1e6]
@@ -163,27 +168,31 @@ function generate_nlp(m, mip_sol)
         JuMP.addNLconstraint(nlp_model, constr_expr)
     end
 
-    @variable(nlp_model, nx_p[i=1:m.num_int_bin_var] >= 0)
-    @variable(nlp_model, nx_m[i=1:m.num_int_bin_var] >= 0)
+    @variable(nlp_model, nabsx[i=1:m.num_int_bin_var] >= 0)
     for i=1:m.num_int_bin_var
         vi = m.int2var_idx[i]
-        @constraint(nlp_model, nx[vi] == mip_sol[vi]+nx_p[i]-nx_m[i])
+        @constraint(nlp_model, nabsx[i] >= nx[vi]-mip_sol[vi])
+        @constraint(nlp_model, nabsx[i] >= -nx[vi]+mip_sol[vi])
     end
-    @objective(nlp_model, Min, sum(nx_p[i]+nx_m[i] for i=1:m.num_int_bin_var))
+    @objective(nlp_model, Min, sum(nabsx[i] for i=1:m.num_int_bin_var))
 
     setsolver(nlp_model, m.nl_solver)
     status = solve(nlp_model)
-    println("Status: ", status)
-    println("Obj: ", getobjectivevalue(nlp_model))
+    println("NLP Status: ", status)
+    println("NLP Obj: ", getobjectivevalue(nlp_model))
     return status, getvalue(nx), getobjectivevalue(nlp_model)
 end
 
+"""
+    generate_real_nlp(m,sol)
 
-function generate_real_nlp(m,sol)
+Generate the orignal nlp and get the objective for that
+"""
+function generate_real_nlp(m, sol)
     rmodel = Model(solver=m.nl_solver)
     lb = [m.l_var; -1e6]
     ub = [m.u_var; 1e6]
-    # all continuous we solve relaxation first
+
     @variable(rmodel, lb[i] <= rx[i=1:m.num_var] <= ub[i])
     for ni=1:m.num_int_bin_var
         i = m.int2var_idx[ni]
@@ -206,28 +215,43 @@ function generate_real_nlp(m,sol)
     return status, getvalue(rx), getobjectivevalue(rmodel)
 end
 
-function add!(t::TabuList, sol)
-    t.sols[t.pointer] = sol
+"""
+    add!(t::TabuList, m, sol)
+
+Add a solution to the tabu list (includes only the discrete variables)
+"""
+function add!(t::TabuList, m, sol)
+    t.sols[t.pointer] = [sol[m.int2var_idx[i]] for i=1:m.num_int_bin_var]
     t.pointer += 1
     if t.pointer > t.length
         t.pointer = 1
     end
 end
 
+"""
+    fpump(m)
+
+Run the feasibility pump 
+"""
 function fpump(m)
     srand(1)
 
     start_fpump = time()
     nlp_sol = m.solution
     nlp_obj = 1 # should be not 0 for while
-    c = 0
+    c = 1
     tabu_list = TabuList()
     mip_sols = Dict{UInt64,Bool}()
     tabu_list.length = 30
     tabu_list.pointer = 1
     tabu_list.sols = []
     for i=1:tabu_list.length
-        push!(tabu_list.sols, NaN*ones(length(nlp_sol))) 
+        push!(tabu_list.sols, NaN*ones(m.num_int_bin_var)) 
+    end
+
+    # construct the linear constraints as a vector of Aff once
+    if m.num_l_constr > 0
+        aff = construct_affine_vector(m)
     end
 
     fix = false
@@ -235,8 +259,9 @@ function fpump(m)
     iscorrect = false
     tl = m.options.feasibility_pump_time_limit
     while !isapprox(nlp_obj,0.0, atol=atol) && time()-start_fpump < tl 
+        # generate a mip or just round if no linear constraints
         if m.num_l_constr > 0
-            mip_status, mip_sol = generate_mip(m, nlp_sol, tabu_list) 
+            mip_status, mip_sol = generate_mip(m, nlp_sol, aff, tabu_list) 
         else
             # if no linear constraints just round the discrete variables
             mip_sol = copy(nlp_sol)
@@ -247,14 +272,25 @@ function fpump(m)
             end
         end
         if mip_status != :Optimal
+            warn("MIP couldn't be solved to optimality")
             break
         end
-        add!(tabu_list, mip_sol)
+        
+        # If a cycle is detected which wasn't able to prevent by the tabu list (maybe too short)
+        if haskey(mip_sols, hash(mip_sol))
+            warn("Cycle detected")
+            break
+        end
+        add!(tabu_list, m, mip_sol)
+        mip_sols[hash(mip_sol)] = true
+
         nlp_status, nlp_sol, nlp_obj = generate_nlp(m, mip_sol)
         if nlp_status != :Optimal
+            warn("NLP couldn't be solved to optimality")
             break
         end
-        if isapprox(nlp_obj, 0.0, atol=1e-4) && nlp_status == :Optimal
+        # if the difference is near 0 => try to improve the obj by using the original obj
+        if isapprox(nlp_obj, 0.0, atol=1e-4)
             real_status,real_sol, real_obj = generate_real_nlp(m, mip_sol)
             if real_status == :Optimal
                 nlp_obj = real_obj
@@ -263,29 +299,14 @@ function fpump(m)
                 break
             end
         end
-        if haskey(mip_sols, hash(mip_sol))
-            warn("Cycle detected")
-        end
-        for i=1:tabu_list.length
-            println([tabu_list.sols[i][m.int2var_idx[j]] for j=1:m.num_int_bin_var])
-        end
-        mip_sols[hash(mip_sol)] = true
         c += 1
-        println("c: ", c)
-        if c >= 3
-            error("1")
-        end
     end
     
     println("It took ", time()-start_fpump, " s")
+    println("It took ", c, " rounds")
     if iscorrect
-        obj = MathProgBase.eval_f(m.d, nlp_sol)
-        println("obj: ", obj)
-        for ni=1:m.num_int_bin_var
-            i = m.int2var_idx[ni]
-        end
-        return nlp_sol, obj
-    end
-    
+        println("Obj: ", nlp_obj)
+        return nlp_sol, nlp_obj
+    end    
     return nothing, nothing
 end
