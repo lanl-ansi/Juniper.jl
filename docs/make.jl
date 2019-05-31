0bbc6aa62ca3aeeeff1dc0ae7a10015101024d34
Ole Kröger <o.kroeger@wikunia.de>
2019-05-31 10:12:24 +0200

    Juniper in Project toml docs, deprecation changes

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,10 @@
 using Documenter, Juniper
 
 makedocs(
-    modules = [Juniper],
-    format = :html,
+    # See https://github.com/JuliaDocs/Documenter.jl/issues/868
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    # See https://github.com/JuliaOpt/JuMP.jl/issues/1576
+    strict = true,
     sitename = "Juniper",
     pages = [
         "Home" => "index.md",
@@ -14,9 +16,5 @@ makedocs(
 )
 
 deploydocs(
-    deps = nothing,
-    make = nothing,
-    target = "build",
     repo = "github.com/lanl-ansi/Juniper.jl.git",
-    julia = "1.0"
 )
