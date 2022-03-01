bd7ef81a73b6c497d46a37379517c9a682c1a857
odow <o.dowson@gmail.com>
2022-03-01 17:31:03 +1300

    Add test for conic model with Ipopt

--- test/conic.jl
+++ test/conic.jl
@@ -3,6 +3,7 @@ module TestConicModels
 using Test
 using JuMP
 
+import Ipopt
 import Juniper
 import SCS
 
@@ -38,6 +39,35 @@ function test_simple_conic_model()
     @test isapprox(value.(x), [6, 8]; atol = 1e-4)
     @test sqrt(value(x[1])^2 + value(x[2])^2) <= 10 + 1e-4
     @test isapprox(objective_value(model), 58; atol = 1e-5)
+    return
+end
+
+function test_simple_conic_model_ipopt()
+    function nl_solver()
+        model = MOI.Bridges.full_bridge_optimizer(Ipopt.Optimizer(), Float64)
+        MOI.Bridges.add_bridge(
+            model,
+            MOI.Bridges.Constraint.SOCtoNonConvexQuadBridge{Float64},
+        )
+        MOI.Bridges.add_bridge(
+            model,
+            MOI.Bridges.Constraint.RSOCtoNonConvexQuadBridge{Float64},
+        )
+        return model
+    end
+    model = Model(Juniper.Optimizer)
+    set_optimizer_attribute(model, "nl_solver", nl_solver)
+    set_silent(model)
+    @variable(model, 0 <= x[1:2] <= 10, Int)
+    @objective(model, Max, 3x[1] + 5x[2])
+    @constraint(model, [10, x[1], x[2]] in SecondOrderCone())
+    optimize!(model)
+    @test termination_status(model) == LOCALLY_SOLVED
+    @test primal_status(model) == FEASIBLE_POINT
+    @test dual_status(model) == FEASIBLE_POINT
+    @test isapprox(value.(x), [6, 8]; atol = 1e-6)
+    @test sqrt(value(x[1])^2 + value(x[2])^2) <= 10 + 1e-6
+    @test isapprox(objective_value(model), 58; atol = 1e-6)
     return
 end
 
